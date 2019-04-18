b4c050a00183dfa2b3de252b3a5835cb2c0b62e7
niiiils <fahrni.nils@gmail.com>
2019-04-18 14:25:33 +0200

    generated components & implemented them into the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeedComponent } from './feed/feed.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'team', component: TeamComponent },
+ { path: 'contact', component: ContactComponent },
+ { path: 'feed', component: FeedComponent },
+ { path: 'projects', component: ProjectsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FeedComponent } from './feed/feed.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent,
+    ProjectsComponent,
+    FeedComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
